f650348c1fc99bf892082ae9acbf1791c85b9603
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-03-24 17:34:18 +0900

    Update extension.cpp

--- extension.cpp
+++ extension.cpp
@@ -1,8 +1,12 @@
-#include "pxt.h"
-
+//% color=#00AAFF weight=100
 namespace fastyield {
+
+    // C++ の関数を TS に宣言
     //% shim=fastyield::yield_now
-    void yield_now() {
-        fiber_sleep(0);
+    declare function yield_now(): void;
+
+    //% block="即時 yield"
+    export function yieldNow(): void {
+        yield_now();
     }
 }
