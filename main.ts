26332f8a5fee5cff8f9730f07e9385407365438d
nagata0318 <y-nagata@duplo-seiko.co.jp>
2026-03-24 17:30:11 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 //% color=#00AAFF weight=100
-//% block="高速yield"
 namespace fastyield {
     //% block="即時 yield"
     export function yieldNow(): void {
-        (fastyield as any).yield_now();
+        yield_now();   // ← C++ 関数名と一致
     }
+
+    // C++ の関数を宣言
+    //% shim=fastyield::yield_now
+    declare function yield_now(): void;
 }
